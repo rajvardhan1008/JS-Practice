910c5f4c41638636704c399d144da56efa45d347
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-14 11:43:08 +0530

    let var and const

---
+++ letVarConst.js
@@ -0,0 +1,25 @@
+//***there are mainly three types of keywords which are used to declare variables - let, var, const
+
+// ***let - block scope
+// {
+//     let number = 5;
+//     console.log(number); // output - 5
+// }
+// console.log(number); // error - undefined
+
+
+// ***var - function scope
+// function printANumber(){
+//     var num = 1;
+//     console.log(num);
+// }
+// printANumber();  // output - 1
+
+// console.log(num); // error - undefined
+
+
+// ***const - used to assign constant values
+// const name = "rajvardhan";
+// console.log(name); // output - "rajvardhan"
+
+// name = "dangi"; // error - Assignment to constant variable
