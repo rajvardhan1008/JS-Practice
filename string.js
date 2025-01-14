f4828b1aaab87605a53db59afe47b22f90f81401
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-14 12:14:18 +0530

    strings in js

---
+++ string.js
@@ -0,0 +1,51 @@
+// var myName = "rajvardhan";
+// // console.log(myName);
+
+// var myRole = "software engineer";
+// var myMarks = 80;
+
+// console.log(myName + myRole + myMarks);
+// // OR 
+// console.log(`hello, my name is ${myName}, my role is ${myRole}, and my marks is ${myMarks}`);
+
+// ***string - it is a collection of characters
+// ***types - primitives and objects
+
+// var greeding = "hello"; //primitive string
+// console.log(typeof greeding); // output - string
+
+// var myName = new String('Raj'); // creates a string object
+// console.log(typeof myName); //output - object
+
+
+// ***some string methods 
+
+// *** slice - to take some part from the original string. slice(start, end); 
+// var str = "hello, i am raj";
+// console.log(str.slice(0,5)); // output - hello
+
+// ***template string - using backtics ``
+// var myName = "rajvardhan";
+// var myRole = "software engineer";
+// var myMarks = 80;
+// console.log(`hello, my name is ${myName}, my role is ${myRole}, and my marks is ${myMarks}`);
+
+// ***split - used to split an array of substrings based on something
+// var str = "hello,my,name,is,rajvardhan,dangi";
+// console.log(str.split(','));
+// var str2 = "hello.this.is.the.demo.repo";
+// console.log(str2.split('.'));
+
+
+// *** replce - used to replace subtring or pattern with another value  
+// var str = "eat an apple and apple";
+// console.log(str.replace('apple', 'banana')); // replaces first occureance
+// console.log(str.replace('apple/g', 'banana')); // replaces all occurances
+
+
+// ***includes - used to find weather a perticualr part is present in a string or not  
+// var str = "hello, duniya";
+// console.log(str.includes('hello')); //true
+// console.log(str.includes('world')); // false
+
+
