52fd0d02b8cf6bb1f13a5ea2b50cc667e6dc46f9
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-15 09:57:45 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,74 @@
+// ***data types - categories of values that determines the type of the data stored in a variable.
+//*** JavaScript supports dynamic typing, which means variables can hold values of different types at different times.
+//*** JS automatically detects the data type of a variable.
+
+
+//***types - Number, Float String, Boolean, Null, Undefined, Object, Array, Function
+
+
+//***Number - Represents neumeric values
+// var num = 5;
+// console.log(num);
+
+
+///***Float - Represents decimal values
+// var n = 1.2;
+// console.log(n);
+
+
+//***String - Collection of characters
+// var str = "hello";
+// var str2 = 'hello';
+// var str3 = "it's hello";
+// var myName = 'Rajvardhan';
+// var str4 = `hello i am ${myName}`;
+// console.log(str);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+
+
+// ***Boolean - Represents true/false values
+// let ans = false;
+// console.log(ans);
+// var cond = true;
+// if(cond) console.log("condition is true");
+// else console.log("consition if false");
+
+
+// ***Null - Represents an intentional absence of a value
+// var nullValue = null;
+// console.log(nullValue);
+
+
+//***Undefined - A variable that is declared but value is not assigned to it
+// var num;
+// console.log(num);
+// console.log(typeof num);
+
+
+// ***Array - Represents list of values   
+// var arr = [1, 2, 3, "hello", "this", "is"];
+// console.log(arr[2]);
+// console.log(arr[4]);
+
+
+// ***Object - Represents a collection of key value pairs
+// const obj = {
+//     name:"Rajvardhan",
+//     place:"Bhopal",
+//     contact:9383583912,
+//     age:21,
+//     grades:[1, 2, 3, 4, 5],
+// };
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.grades[2]);
+
+
+// ***Function - Resuable block of Code
+// function greet(){
+//     console.log("namaste, duniya");
+// }
+// greet();
+// console.log(typeof(greet));
