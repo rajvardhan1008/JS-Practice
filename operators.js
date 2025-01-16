66c1faa68df4d7e988099da59feb5db065d8e8bf
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-16 10:19:44 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,90 @@
+// ***Operators are used to perform mathematical and logical computations. 
+
+
+// ***Assignment operator (=) : assigns a value in the varible. 
+// var a=5;
+// var b;
+// b=2;
+// console.log(a);
+// console.log(b);
+
+
+// ***Addition(+) : add values 
+// let num1 = 5;
+// let num2 = 6;
+// let str = "raj";
+// let num3 = num1+num2;
+// console.log(num1+num2);
+// console.log(num1+raj);
+// console.log(num3);
+
+
+//***Multiplication(*) : multiplies values
+// let a = 3;
+// let b = 2;
+// console.log(a*b);
+
+
+//***Division(/) : divide values
+// let a = 9;
+// let b = 2;
+// console.log(a/b);
+
+
+//***Substraction(-) : subtract values
+// let num1 = 10;
+// console.log(num1-2);
+
+
+// ***Modulus(%) : gives reminder   
+// let a = 41;
+// let b = 6;
+// console.log(a%b);
+
+
+//***Add and Assign(+=) 
+// let a = 10;
+// console.log(a);
+// a+=5; // adds 5 to a, now a = 15;
+// console.log(a);
+
+
+// ***Substract and Assign(-=)
+// let num = 30;
+// num -= 10;
+// console.log(num);
+
+
+//***Equal(==) compares the values
+// let a = 10;
+// let b = 11;
+// let c = 11;
+// if(b == c){
+//     console.log("a==b");
+// }
+// else{
+//     console.log("a != b")
+// }
+
+
+// ***Strict Equal(===) : compares values and data types 
+// let a = "5"; // string
+// let b = 5; //number
+// console.log(a==b);
+// console.log(a===b);
+
+
+// ***Greater than(>) : compares if the values left side is greater than right side or not 
+// let a = 10;
+// let b = 5;
+// console.log(a>b);
+
+
+// ***Logical And(&&) : both left and right side statements should be true 
+// let a = 20;
+// let b = 10;
+// let c = 5;
+// console.log((a>b) && (b>c));
+
+// *** Logical OR(||) : any one statement should be true
+// console.log((5>2) || (10>2));
