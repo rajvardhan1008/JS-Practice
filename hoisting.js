429aad92f21c45083090484cdad5b2703ca9b264
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-16 10:27:34 +0530

    let var const hoisting

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+// *** Hoisting is a process of where variable declaration move to the top of the scope in the 
+// phase of execution time
+
+//***var hoisting : initialized with undefined values;
+// console.log(a);
+// var a = 5;
+
+
+// ***Let hoisting : hoisted but not initialized with any value 
+// console.log(num); // Error
+// let num = 6;
+
+//const hoising : same as let
+// console.log(num);
+// const num = 10;
