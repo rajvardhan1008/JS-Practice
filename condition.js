aaa95e8620ad6309df7ee03effd3ebe330564361
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-17 19:08:42 +0530

    conditions

---
+++ condition.js
@@ -0,0 +1,68 @@
+// ***conditional operator are executed on the basis on some conditons 
+
+// ***types : if, if-else, else-if, ternary operator, switch cases
+
+// ***if : exucuted when the contion is true 
+// if(10>6){
+//     console.log("greater");
+// }
+
+
+// ***if-else : when 'if' is not executed else will
+// ***without if, else condition can not be executed
+// if(10>20){
+//     console.log("greater");
+// }
+// else{
+//     console.log("smaller");
+// }
+
+
+// ***else if : if 'if' condition is not executed it will go to else if 
+//***there can be multiple else-if conditions
+// if(10 > 10){
+//     console.log("greater");
+// }
+// else if(10 == 10){
+//     console.log("equal");
+// }
+// else{
+//     console.log("smaller");
+// }
+
+
+//***ternary operator - shortest way to write conditions
+// condition ? true_case : false_case
+// console.log(5 > 2 ? "greater" : "smaller");
+
+
+// ***switch case :allows for multiple conditional branches based on the value of an expression.
+// var day = 3;
+// let dayName;
+// switch(day){
+//     case 1:
+//         dayName = "Monday";
+//         break;
+//     case 2:
+//         dayName = "Tuesday";
+//         break;
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break;
+//     case 7:
+//         dayName = "Sunday";
+//         break;
+//     default:
+//         dayName = "Invalid";
+// }
+
+// console.log(dayName);
