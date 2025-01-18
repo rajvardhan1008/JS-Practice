18a88e7845161d50be84684ad9be84e47dfe37ee
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-18 08:59:23 +0530

    for loop

---
+++ loops.js
@@ -0,0 +1,12 @@
+//***loop is a block of code which exucutes untill a specific condition meet
+// ***types - for loop, while loop, do while loop, forEach loop, for in loop, for of loop 
+
+
+// ***for loop - used when number of iterations is known beforehand.
+// ***for(start; end; condition)
+//***printing from 1 to 10
+// for(let i=1; i<11; i++){
+//     console.log(i);
+// }
+
+
