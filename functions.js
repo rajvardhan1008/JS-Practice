ee1b57825e25db32ed73bae21833dc65619f79fc
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-20 09:52:51 +0530

    parameters and arguments in js

---
+++ functions.js
@@ -0,0 +1,16 @@
+// ***Functions - reusable code of block to perform specif task, making code more readable, maintainable and modular
+
+
+// ***Parameter - name of the values which is expected by the function when it is called
+// function multiply(a, b){ // a and b are paramenters
+//     return a*b;
+// }
+// const ans = multiply(2,8);
+// console.log(ans);
+
+
+// ***Arguments - values which are passed to the funtion when it is called
+// function add(a, b){
+//     console.log(a+b);
+// }
+// add(5, 2); // 5 and 2 are arguments
