e46db2b5655b240783b3a7d626653f0e0f78b8c3
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-21 19:16:18 +0530

    positined arguments

---
+++ arguments.js
@@ -0,0 +1,10 @@
+// ***Arguments - these are actual values which are passed to the function when it is called.
+
+// ***Positioned arguments - arguments are passed in the order they are defined in the function.
+
+// function greet(firstName, lastName){
+//     console.log(`Namaste, ${firstName} ${lastName}`);
+// }
+// greet("Raj", "Dangi");
+
+
