dde8213a4f3caa697b8eb2f79ea63347fd84359b
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-21 19:21:28 +0530

    rest parameters

--- parameters.js
+++ parameters.js
@@ -9,3 +9,10 @@
 // greet() // namaster, undefined
 
 
+// ***Rest parameters - allows funtion to accept indefinite number of arguments
+// function sumOfArray(...numbers){ // ...numbers are rest parameters
+//     return numbers.reduce((acc, num) => acc+num, 0);
+// }
+// console.log(sumOfArray(1,2,3,4,5,6,7,8,9,10));
+
+
