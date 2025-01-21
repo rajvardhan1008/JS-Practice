f8f59fc58dcd6907772b4e353fb80547a3daaaf6
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-21 19:20:33 +0530

    required parameter

---
+++ parameters.js
@@ -0,0 +1,11 @@
+// ***Parameters - allows us to accept inputs and executes login basis on them in js
+
+
+// ***Required Parameters - these parameters must be provided to the function, if not then value will be undefined
+// function greet(name){
+//     console.log(`namaste, ${name}`);
+// }
+// greet("rajvardhan") // namaste, rajvardhan
+// greet() // namaster, undefined
+
+
