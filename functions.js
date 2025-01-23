b09cd163f43aabbe962035b53ac7de2d7bbe9783
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2025-01-23 13:22:44 +0530

    classic function

--- functions.js
+++ functions.js
@@ -22,3 +22,10 @@
 // }
 // console.log(sumOfArray(1,2,3,4,5,6,7,8,9,10));
 
+
+// ***Classic Function - standard way of declaring functions
+// function sayNamaste(){
+//     console.log("Namaste, Duniya");
+// }
+// sayNamaste();
+
